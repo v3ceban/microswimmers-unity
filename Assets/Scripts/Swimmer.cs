ea3b61149bf7c99d5351600b112e9712f739b43b
v3ceban <v3ceban@gmail.com>
2023-01-04 00:47:22 -0800

    animation progress

--- Assets/Scripts/Swimmer.cs
+++ Assets/Scripts/Swimmer.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class Swimmer : MonoBehaviour
 {
-    private float movementX;
-    private float Displacement;
-    private int Animation;
-    private string PreviousKey;
-    private string PreviousKey2;
-    private string PreviousKey3;
     private Rigidbody2D myBody;
-
     private SpriteRenderer sr;
-
     private Animator anim;
+
+    private float movementX; //set later to Input.GetAxisRaw("Horizontal"), which basically sets it to 1 when D(d) or Rigth Arrow is pressed and to -1 when A(a) or Left arrow is pressed; used to change direction on different button presses, may be removed later
+    private float Displacement = 1f; //arbitrary value of 1 for now
+    private bool Animation; //boolean value of animation condition
+    private bool Rext = true; //Default Right Sphere animation condition
+    private bool Lext = true; //Default Left Sphere animation condition
 
     private void Awake() {
         myBody = GetComponent<Rigidbody2D>();
@@ -34,99 +32,36 @@ public class Swimmer : MonoBehaviour
         PlayerInput();
     }
 
-    void PlayerMove(float x) {
-        //movementX = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(x, 0f, 0f);
+    void SwimmerMove(float x) {
+        movementX = Input.GetAxisRaw("Horizontal");
+        //transform.position += new Vector3(x, 0f, 0f) * movementX; //moves the swimmer
     }
 
-    void SwimmerAnimate(int i) {
-        anim.SetInteger("i", i);
+    //function to animate left sphere
+    void LsphereAnimate(string Extended) {
+        //right sphere always extended, left sphere moves
+        if (Lext == true && Rext == true) {
+            anim.SetBool(Extended, false);
+            Lext = false;
+        } else if (Lext == false && Rext == true) {
+            anim.SetBool(Extended, true);
+            Lext = true;
+        }
     }
 
-    void CheckKeyInputs(string Key) {
-        if (Key == "Left Arrow" && PreviousKey == null && PreviousKey2 == null && PreviousKey3 == null) {
-                PreviousKey = Key;
-                PreviousKey2 = null;
-                PreviousKey3 = null;
-                Animation = 1;
-                Displacement = -1.3531f;
-            } else if (Key == "Left Arrow" && PreviousKey == "Left Arrow" && PreviousKey2 == null && PreviousKey3 == null) {
-                PreviousKey = null;
-                PreviousKey2 = null;
-                PreviousKey3 = null;
-                Animation = 0;
-                Displacement = 1.3531f;
-            } else if (Key == "Right Arrow" && PreviousKey == "Left Arrow" && PreviousKey2 == null && PreviousKey3 == null) {
-                PreviousKey = Key;
-                PreviousKey2 = "Left Arrow";
-                PreviousKey3 = null;
-                Animation = 2;
-                Displacement = 1.4391f;
-            } else if (Key == "Right Arrow" && PreviousKey == "Right Arrow" && PreviousKey2 == "Left Arrow" && PreviousKey3 == null) {
-                PreviousKey = "Left Arrow";
-                PreviousKey2 = null;
-                PreviousKey3 = null;
-                Animation = 1;
-                Displacement = -1.4391f;
-            } else if (Key == "Left Arrow" && PreviousKey == "Right Arrow" && PreviousKey2 == "Left Arrow" && PreviousKey3 == null) {
-                PreviousKey = Key;
-                PreviousKey2 = "Right Arrow";
-                PreviousKey3 = "Left Arrow";
-                Animation = 3;
-                Displacement = 1.4391f;
-            } else if (Key == "Left Arrow" && PreviousKey == "Left Arrow" && PreviousKey2 == "Right Arrow" && PreviousKey3 == "Left Arrow") {
-                PreviousKey = "Right Arrow";
-                PreviousKey2 = "Left Arrow";
-                PreviousKey3 = null;
-                Animation = 2;
-                Displacement = -1.4391f;
-            } else if (Key == "Right Arrow" && PreviousKey == "Left Arrow" && PreviousKey2 == "Right Arrow" && PreviousKey3 == "Left Arrow") {
-                PreviousKey = Key;
-                PreviousKey2 = "Left Arrow";
-                PreviousKey3 = "Right Arrow";
-                Animation = 4;
-                Displacement = -1.3531f;
-            } else if (Key == "Left Arrow" && PreviousKey == "Right Arrow" && PreviousKey2 == "Left Arrow" && PreviousKey3 == "Right Arrow") {
-                PreviousKey = null;
-                PreviousKey2 = null;
-                PreviousKey3 = null;
-                Animation = 0;
-            }
+     //function to animate right sphere
+    void RsphereAnimate(string Extended) {
+        print(Rext + " " + Lext);
     }
 
+    //run movement and animation functions based on key input;
     void PlayerInput() {
         if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            CheckKeyInputs("Right Arrow");
-            PlayerMove(Displacement);
-            SwimmerAnimate(Animation);
+            SwimmerMove(Displacement);
+            RsphereAnimate("RsphereExtended");
         } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            CheckKeyInputs("Left Arrow");
-            PlayerMove(Displacement);
-            SwimmerAnimate(Animation);
+            SwimmerMove(Displacement);
+            LsphereAnimate("LsphereExtended");
         }
     }
 }
-
-// else if (Key == "Right Arrow" && PreviousKey == null) {
-//                 PreviousKey = Key;
-//                 sr.flipX = true;
-//                 Animation = 1;
-//                 Displacement = 1.3531f;
-//             } else if (Key == "Left Arrow" && PreviousKey == "Left Arrow") {
-//                 PreviousKey = null;
-//                 PreviousKey2 = null;
-//                 Animation = 0;
-//                 Displacement = 1.3531f;
-//             } else if (Key == "Right Arrow" && PreviousKey == "Right Arrow") {
-//                 PreviousKey = null;
-//                 PreviousKey2 = null;
-//                 sr.flipX = true;
-//                 Animation = 0;
-//                 Displacement = -1.3531f;
-//              } 
-// else if (Key == "Left Arrow" && PreviousKey == "Right Arrow" && PreviousKey2 == "Left Arrow") {
-//                 PreviousKey = null;
-//                 PreviousKey2 = null;
-//                 Animation = 0;
-//                 Displacement = 0;
-//             }
